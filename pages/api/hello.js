879d9395a62b04b8cce0b1375a1657214d8ffd45
Jorne Saenen <59235736+JorneSaenen@users.noreply.github.com>
2022-06-08 21:49:33 +0200

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,34 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import sgMail from "@sendgrid/mail";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const data = req.body;
+  const msg = {
+    from: process.env.SENDGRID_FROM_EMAIL,
+    template_id: process.env.SENDGRID_TEMPLATE_ID,
+    personalizations: [
+      {
+        to: [
+          {
+            email: process.env.SENDGRID_TO_EMAIL,
+          },
+          {
+            email: data.email,
+          },
+        ],
+        dynamic_template_data: {
+          ...data,
+          date: new Date().toLocaleDateString("nl-BE"),
+        },
+      },
+    ],
+  };
+  JSON.stringify(msg.personalizations);
+  try {
+    await sgMail.send(msg);
+    res.status(201).json({ message: "Email succesfully send!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to send the email." });
+  }
 }
